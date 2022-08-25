81335dc4a3216a392658bb0f0e5575dfa253cec2
Mansour Achaour <achaourmansour@gmail.com>
2022-08-25 09:50:49 +0200

    Avancemnt de projet crm utilisation ngIf ngClass

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IconDeleteComponent } from './icons/components/icon-delete/icon-delete.component';
+import { PageForgotPasswordComponent } from './login/pages/page-forgot-password/page-forgot-password.component';
+import { PageResetPasswordComponent } from './login/pages/page-reset-password/page-reset-password.component';
+import { PageSignInComponent } from './login/pages/page-sign-in/page-sign-in.component';
+import { PageSignUpComponent } from './login/pages/page-sign-up/page-sign-up.component';
+import { PageListOrdersComponent } from './orders/pages/page-list-orders/page-list-orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/sign-in', pathMatch: 'full' },
+  {path: 'sign-in', component: PageSignInComponent},
+  {path: 'sign-up', component: PageSignUpComponent},
+  {path: 'reset', component: PageResetPasswordComponent},
+  {path: 'forgot', component: PageForgotPasswordComponent},
+// lazy loading
+  {
+    path: 'orders',
+    loadChildren: () =>
+      import('./orders/orders.module').then((m) => m.OrdersModule),
+  },
+  {
+    path: 'clients',
+    loadChildren: () =>
+      import('./clients/clients.module').then((m) => m.ClientsModule),
+  },
+
+  //ROUTES SIGN UP
+  //RESET
+  //FORGOT
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/orders/orders-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PageAddOrderComponent } from './pages/page-add-order/page-add-order.component';
+import { PageEditOrderComponent } from './pages/page-edit-order/page-edit-order.component';
+import { PageListOrdersComponent } from './pages/page-list-orders/page-list-orders.component';
+
+const routes: Routes = [
+
+  //localhost:4200/orders
+{path: '',component: PageListOrdersComponent},
+{path: 'add',component: PageAddOrderComponent},
+{path: 'edit',component:PageEditOrderComponent}
+
+// localhost:4200/orders/add
+// localhost:4200/orders/edit
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class OrdersRoutingModule { }
